d5694c0b41b72ff116a2f9d944adfed1004ecbb4
Kobas <kobas394@gmail.com>
2020-04-19 12:11:37 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin')
 module.exports = {
   publicPath: '/my-first-project/', //todo: configure githubpages
   configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: [ '/', '/about' ],
-      })
-    ]
+    // plugins: [
+    //   new PrerenderSPAPlugin({
+    //     // Required - The path to the webpack-outputted app to prerender.
+    //     staticDir: path.join(__dirname, 'dist'),
+    //     // Required - Routes to render.
+    //     routes: [ '/', '/about' ],
+    //   })
+    // ]
   },
 
   pluginOptions: {
